bc483fd3259fc5539fdc04b7a8e52208c219539c
juyoung07 <yunju5090@gmail.com>
2025-03-24 12:19:16 +0900

    first spring boot

---
+++ FirstBoot/src/test/java/kr/hs/study/FirstBoot/FirstBootApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hs.study.FirstBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

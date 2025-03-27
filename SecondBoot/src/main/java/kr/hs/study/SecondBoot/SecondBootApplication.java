01c2e5311b9bedec8a7ca945a83999d7f8dde7db
juyoung07 <yunju5090@gmail.com>
2025-03-28 08:31:51 +0900

    thymeleaf

---
+++ SecondBoot/src/main/java/kr/hs/study/SecondBoot/SecondBootApplication.java
@@ -0,0 +1,13 @@
+package kr.hs.study.SecondBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondBootApplication.class, args);
+	}
+
+}

2044d9f1d9838fa9221a8eaea01ca84b88d82773
juyoung07 <yunju5090@gmail.com>
2025-03-31 11:39:10 +0900

    WebRequest

--- GetParameter/src/main/java/kr/hs/school/GetParameter/controller/TestController.java
+++ GetParameter/src/main/java/kr/hs/school/GetParameter/controller/TestController.java
@@ -3,11 +3,12 @@ package kr.hs.school.GetParameter.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.context.request.WebRequest;
 
 @Controller
 public class TestController {
     @GetMapping("/test1")
-    public String test1(HttpServletRequest req) {
+    public String test1(WebRequest req) {
         String str1 = req.getParameter("data1");
         String str2 = req.getParameter("data2");
         System.out.println("data1 : " + str1);

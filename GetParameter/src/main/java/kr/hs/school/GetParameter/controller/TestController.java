3ba03e61d3985386e213fb89bec38471cb3efde4
juyoung07 <yunju5090@gmail.com>
2025-03-31 11:59:05 +0900

    RequestParam

--- GetParameter/src/main/java/kr/hs/school/GetParameter/controller/TestController.java
+++ GetParameter/src/main/java/kr/hs/school/GetParameter/controller/TestController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.WebRequest;
 
 @Controller
@@ -40,6 +41,19 @@ public class TestController {
         System.out.println("data2 : " + data2);
         System.out.println("data3 : " + data3);
 
+        return "result";
+    }
+
+    @GetMapping("/test4")
+    public  String test4(@RequestParam("data1") int data1,
+                         @RequestParam("data2") int data2,
+                         @RequestParam("data3") int data3[]) {
+
+        System.out.println("data1 : " + data1);
+        System.out.println("data2 : " + data2);
+        for (int a : data3) {
+            System.out.println("data3 : " + a);
+        }
         return "result";
     }
 }

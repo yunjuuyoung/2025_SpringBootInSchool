f66403d547eff68bcafb8c6a40107b75f863d870
juyoung07 <yunju5090@gmail.com>
2025-03-31 11:53:49 +0900

    PathVariable

--- GetParameter/src/main/java/kr/hs/school/GetParameter/controller/TestController.java
+++ GetParameter/src/main/java/kr/hs/school/GetParameter/controller/TestController.java
@@ -3,16 +3,43 @@ package kr.hs.school.GetParameter.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.context.request.WebRequest;
 
 @Controller
 public class TestController {
     @GetMapping("/test1")
-    public String test1(WebRequest req) {
+    public String test1(HttpServletRequest req) {
         String str1 = req.getParameter("data1");
         String str2 = req.getParameter("data2");
+        String str3[] = req.getParameterValues("data3");  //  배열로 받기
+
         System.out.println("data1 : " + str1);
         System.out.println("data2 : " + str2);
+        for (String a : str3) {
+            System.out.println("data3 : " + a);
+        }
+        return "result";
+    }
+
+    @GetMapping("/test2")
+    public String test2(WebRequest req) {
+        String str1 = req.getParameter("data1");
+        String str2 = req.getParameter("data2");
+
+        System.out.println("data1 : " + str1);
+        System.out.println("data2 : " + str2);
+        return "result";
+    }
+
+    @GetMapping("/test3/{data1}/{data2}/{data3}")
+    public String test3(@PathVariable("data1") int data1,
+                        @PathVariable("data2") int data2,
+                        @PathVariable("data3") int data3) {
+        System.out.println("data1 : " + data1);
+        System.out.println("data2 : " + data2);
+        System.out.println("data3 : " + data3);
+
         return "result";
     }
 }
